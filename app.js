5dfe9045b944541514fa9f0938d84b37c3c5aa77
Shubham Anand <shubhamrocksdoa13@gmail.com>
2024-11-30 10:03:39 +0530

    episode 2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
 const heading = React.createElement(
     "div", { id:"parent"}, 
     [
@@ -9,7 +13,7 @@ const heading = React.createElement(
         React.createElement("div", {id:"child2"}, 
         [
         React.createElement("h1", {}, "This is an H1 Tag"), 
-        React.createElement("h2", {}, "This is an H2 Tag")
+        React.createElement("h2", {}, "This is an H3 Tag")
         ])
     ]
     );
@@ -18,4 +22,3 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(root)
 root.render(heading);
  // transitive dependency - We have only downloaded parcel but in node modules it showed so many files, because parcel as a project had other dependencies and other dependecies had other other dependencies
- 
