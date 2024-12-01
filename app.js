0e10b8529618c0c41d0aec3f9f411ac8a3078cb4
Shubham Anand <shubhamrocksdoa13@gmail.com>
2024-12-01 23:04:35 +0530

    Adding JSX for the first time

--- app.js
+++ app.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom';
 
 
 const heading = React.createElement(
-    "div", { id:"parent"}, 
-    [
-        React.createElement("div", {id:"child1"}, 
-        [
-        React.createElement("h1", {}, "This is an H1 Tag"), 
-        React.createElement("h2", {}, "This is an H2 Tag")
-        ]),
-        React.createElement("div", {id:"child2"}, 
-        [
-        React.createElement("h1", {}, "This is an H1 Tag"), 
-        React.createElement("h2", {}, "This is an H3 Tag")
-        ])
-    ]
+    "div", { id:"parent"}, "This is test"
     );
-console.log(heading)    
+   
+let testJSX = <h1>This is loading from JSX</h1>    
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root)
-root.render(heading);
+
+root.render(testJSX);
  // transitive dependency - We have only downloaded parcel but in node modules it showed so many files, because parcel as a project had other dependencies and other dependecies had other other dependencies
